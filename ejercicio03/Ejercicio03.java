17f56b84b18ffe7747cf41a4d405bc9b43878149
Javier <javier.mf.1996@gmail.com>
2016-11-07 19:17:13 +0100

    ejercicio3

---
+++ ejercicio03/Ejercicio03.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Ejercicio03 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);
+		int N, divisor = 1;
+		
+		System.out.print("Divisores de: ");
+		N=teclado.nextInt();
+		
+		while (divisor < N){	
+			if ( N % divisor == 0){
+				
+				
+				System.out.println(divisor);
+				
+			}
+			
+			divisor++;		
+		}
+				
+				
+				
+		
+			 
+	}
+			
+}
+
+
